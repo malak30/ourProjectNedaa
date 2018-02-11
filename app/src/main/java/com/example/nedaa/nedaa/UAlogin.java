50a8a67cb6996d5d6f0e52a1d1953faeffe9f96d
malak <ma_80@outlook.com>
2018-02-11 20:29:41 +0300

    comment

---
+++ app/src/main/java/com/example/nedaa/nedaa/UAlogin.java
@@ -0,0 +1,13 @@
+package com.example.nedaa.nedaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UAlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ualogin);
+    }
+}
